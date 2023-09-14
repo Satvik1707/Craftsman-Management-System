c6f1287640ae20cbb58e46dc81d91a10bce4291b
Satvik Madaan <satvik0125@gmail.com>
2023-09-15 01:39:50 +0530

    last commit

---
+++ Server/config/db.js
@@ -0,0 +1,21 @@
+const { Client } = require('pg');
+require('dotenv').config();
+
+// Construct the connection string based on the environment
+const connectionString =
+  process.env.NODE_ENV === 'production'
+    ? `postgres://satvik0125:pVoFMOxX94Na@ep-flat-moon-36733642.us-east-2.aws.neon.tech/Test1`
+    : `postgres://satvik0125:pVoFMOxX94Na@ep-flat-moon-36733642.us-east-2.aws.neon.tech/Test1`;
+
+// Create a new PostgreSQL client
+const pool = new Client({
+  connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: true, // Set to false if your server uses a self-signed certificate
+  },
+});
+
+// Connect to the PostgreSQL database
+pool.connect();
+
+module.exports = { pool };
